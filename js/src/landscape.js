fdede23d14a9b7e3bb8362356693fe164278070b
rvounik <robbin@vanooij.nl>
2016-03-28 20:20:07 +0200

    finished set up that is supposed to work, but doesnt

--- js/src/landscape.js
+++ js/src/landscape.js
@@ -1,25 +1,18 @@
-//import createjs from 'createjs'; //https://github.com/CreateJS/PreloadJS/issues/85
-//import canvas from 'canvas';
-//import createjs from 'node-easel';
-//import renderTree from './partials/renderTree';
-//import userControls from './partials/userControls';
-
 import $ from 'jquery';
-import createjs from 'createjs';
-import easeljs from 'easeljs';
+
+//import easeljs from 'easeljs/lib/easeljs-0.8.2.combined';
 
 ((config) => {
+
     $(document).ready(() => {
 
-    // quick test
-    var stage = new createjs.Stage(config.selector.canvas);
-    var shape = new createjs.Shape();
-    shape.graphics.beginFill('red').drawRect(0, 0, 120, 120);
-    stage.addChild(shape);
-    stage.update();
-
-    // load bitmap of heightmap to canvas object using createjs
-    // with the exact id handed over by an appConfig parameter
+        // quick test
+        var stage = new createjs.Stage(config.selector.canvas);
+        var shape = new createjs.Shape();
+        shape.graphics.beginFill('red').drawRect(100, 100, 100, 100);
+        stage.addChild(shape);
+        stage.update();
 
     });
+
 })(appConfig);
